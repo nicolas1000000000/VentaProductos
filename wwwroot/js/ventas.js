a51c253e4d6a92ce75aed6ec1c43d47fb1e4b251
Nico <nikorista10@gmail.com>
2024-10-28 22:00:05 -0300

    Parcial

--- wwwroot/js/ventas.js
+++ wwwroot/js/ventas.js
@@ -1,10 +1,1 @@
-function ObtenerInscripciones() {
-    fetch('https://localhost:7245/Ventas')
-    .then(result => result.json())
-    .then(data => MostrarVentas(data))
-    .catch(error => console.log("No se pudo acceder al servicio.", error));
-}
 
-function MostrarVetass(data) {
-    $("#todasLasVentas").empty();
-    
